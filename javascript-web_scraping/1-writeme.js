0617d96f279a7d167633721a19405b2b6c545fc6
akka97 <akapellari@yahoo.com>
2023-09-19 15:52:42 +0200

    tasks

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-fs.writeFile(process.argv[2], 'utf-8', (err, data) => {
+fs.writeFile(process.argv[], 'utf-8', (err, data) => {
     if (err) throw err;
+    console.log(data);
 });
