c5d8112ed72df2652a6d6e738e8e0b243ebb8b0b
akka97 <akapellari@yahoo.com>
2023-09-21 04:16:22 -0700

    add

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process')
+const process = require('process');
 
 const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
