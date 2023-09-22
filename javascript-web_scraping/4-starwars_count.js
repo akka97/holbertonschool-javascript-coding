248a327f217729b759a3714644c71565c853a7b1
akka97 <akapellari@yahoo.com>
2023-09-22 02:59:50 -0700

    add

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const process = require('process');
 const request = require('request');
 const api = process.argv[2];
@@ -19,23 +18,3 @@ request(api, function (error, response, body) {
   });
   console.log(filtered.length);
 });
-=======
-
-const axios = require('axios');
-let count = 0;
-
-axios.get(process.argv[2])
-  .then(function (response) {
-    response.data.results.forEach(element => {
-      element.characters.forEach(movies => {
-	if (movies.includes('18')) {
-	  count++;
-	}
-      });
-    });
-    console.log(count);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
->>>>>>> 041022ee41bc80e437f3211a2813c1e73e96db4d
