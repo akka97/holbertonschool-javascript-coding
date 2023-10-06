6d6e1f8bc33609d02e1cdd4cfe075af16457784b
akka97 <akapellari@yahoo.com>
2023-10-06 11:20:41 +0200

    add

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,25 +8,25 @@ function countStudents(path) {
 
     console.log(`Number of students: ${lines.length}`);
 
-  const fieldObj = {};
+    const fieldObj = {};
     for (const line of lines) {
-        const fields = line.split(',');
-        const field = fields[fields.length - 1];
+      const fields = line.split(',');
+      const field = fields[fields.length - 1];
 
-        if (fieldObj[field]) {
-            fieldObj[field] += 1;
-        } else {
-            fieldObj[field] = 1;
-        }
+      if (fieldObj[field]) {
+        fieldObj[field] += 1;
+      } else {
+        fieldObj[field] = 1;
+      }
     }
 
     for (const [field, count] of Object.entries(fieldObj)) {
-        const st = lines.filter((line) => line.endsWith(field))
-            .map((line) => line.split(',')[0])
-            .join(', ');
-        console.log(`Number of students in ${field}: ${count}. List: ${st}`);
+      const st = lines.filter((line) => line.endsWith(field))
+        .map((line) => line.split(',')[0])
+        .join(', ');
+      console.log(`Number of students in ${field}: ${count}. List: ${st}`);
     }
-} catch (error) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
